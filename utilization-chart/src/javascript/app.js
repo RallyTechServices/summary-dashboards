ea21484eb72719051e2cde0ea7f94ae84614d6cd
John Martin <curmudgeon+git@rallydev.com>
2015-07-19 18:00:58 -0700

    roll up, including array values

--- utilization-chart/src/javascript/app.js
+++ utilization-chart/src/javascript/app.js
@@ -205,6 +205,13 @@ Ext.define("utilization-chart", {
                     var parent_oid = parent.ObjectID;
                     parent_iterations = Ext.Array.merge(parent_iterations, this.iterations_by_project_oid[parent_oid]);
                 }
+                
+                this._setValuesForParent(leaf.get('Project'),'__startScope');
+                this._setValuesForParent(leaf.get('Project'),'__endScope');
+                this._setValuesForParent(leaf.get('Project'),'__endAcceptance');
+
+                this._setArrayValuesForParent(leaf.get('Project'),'__dailyScope');
+                this._setArrayValuesForParent(leaf.get('Project'),'__dailyAcceptance');
             },this);
             
             console.log('parents:', parent_iterations);
@@ -212,6 +219,37 @@ Ext.define("utilization-chart", {
         }
         
         return iterations;
+    },
+    
+    _setArrayValuesForParent: function(leaf,field) {
+        if (! leaf.Parent ) {
+            return null;
+        }
+        
+        var leaf_oid = leaf.ObjectID;
+        var parent_oid = leaf.Parent.ObjectID;
+        var leaf_iteration = this.iterations_by_project_oid[leaf_oid];
+        var parent_iteration = this.iterations_by_project_oid[parent_oid];
+        
+        if ( parent_iteration ) {
+            var parent_values = parent_iteration.get(field) || [];
+            var leaf_values = leaf_iteration.get(field) || [];
+            
+            var new_values = [];
+            Ext.Array.each(leaf_values, function(leaf_value,idx){
+                var value = leaf_value || 0;
+                if ( idx < parent_values.length ) {
+                    var parent_value = parent_values[idx] || 0;
+                    new_values.push(value + parent_value);
+                } else {
+                    new_values.push(leaf_value);
+                }
+            });
+            parent_iteration.set(field, new_values);
+            return parent_iteration.get('Project');
+        }
+        
+        return null;
     },
     
     _setValuesForParent: function(leaf,field) {
@@ -225,10 +263,10 @@ Ext.define("utilization-chart", {
         var parent_iteration = this.iterations_by_project_oid[parent_oid];
         
         if ( parent_iteration ) {
-            var parent_planned = parent_iteration.get(field) || 0;
-            var leaf_planned = leaf_iteration.get(field) || 0;
+            var parent_value = parent_iteration.get(field) || 0;
+            var leaf_value = leaf_iteration.get(field) || 0;
             
-            parent_iteration.set(field, parent_planned + leaf_planned);
+            parent_iteration.set(field, parent_value + leaf_value);
             return parent_iteration.get('Project');
         }
         

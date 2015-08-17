a0dec36700afc1660af47e1bbf32e71bc9208ba3
kcorkan <kcorkan@rallydev.com>
2015-08-17 08:39:14 -0600

    removed console.log

--- wip-limits-chart/src/javascript/__ts-info-link.js
+++ wip-limits-chart/src/javascript/__ts-info-link.js
@@ -63,7 +63,6 @@ Ext.define('Rally.technicalservices.InfoLink',{
                 text = response.responseText;
                 if ( CHECKSUM ) {
                     if ( CHECKSUM !== me._generateChecksum(text) ) {
-                        console.log("Checksums don't match!");
                         deferred.resolve(false);
                         return;
                     }
@@ -93,7 +92,6 @@ Ext.define('Rally.technicalservices.InfoLink',{
                     }
                 },
                 failure: function(msg){
-                    console.log("oops:",msg);
                 }
             });
         } else {

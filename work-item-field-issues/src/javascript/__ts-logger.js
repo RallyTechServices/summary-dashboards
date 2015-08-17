35fb81b8ad61e04b3350fed44728affd123720e9
John Martin <curmudgeon+git@rallydev.com>
2015-08-17 08:58:28 -0700

    change logger to not console if isIE9 (or less)

--- work-item-field-issues/src/javascript/__ts-logger.js
+++ work-item-field-issues/src/javascript/__ts-logger.js
@@ -1,16 +1,19 @@
+
 /*
  */
 Ext.define('Rally.technicalservices.Logger',{
     constructor: function(config){
         Ext.apply(this,config);
     },
-    log: function(){
-        var timestamp = "[ " + Ext.util.Format.date(new Date(), "Y-m-d H:i:s.u") + " ]";
-        var i = -1, l = arguments.length, args = [], fn = 'console.log(args)';
-        while(++i<l){
-            args.push('args['+i+']');
-        };
-        fn = new Function('args',fn.replace(/args/,args.join(',')));
-        fn(arguments);
+    log: function(args){
+        if ( !Ext.isIE9m ) {
+            var timestamp = "[ " + Ext.util.Format.date(new Date(), "Y-m-d H:i:s.u") + " ]";
+            var output_args = [];
+            output_args = Ext.Array.push(output_args,[timestamp]);
+            output_args = Ext.Array.push(output_args, Ext.Array.slice(arguments,0));
+    
+            window.console && console.log.apply(console,output_args);
+        }
     }
+
 });

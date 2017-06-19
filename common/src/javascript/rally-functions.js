3adbde0434c2e261fc07aa1e3ea8ae292ec5e084
John Martin <curmudgeon+git@rallydev.com>
2017-06-18 19:59:32 -0700

    restrict release to only get stories from features in the release regardless of the story's release

--- common/src/javascript/rally-functions.js
+++ common/src/javascript/rally-functions.js
@@ -1,5 +1,5 @@
-/** this class is configured with { series : [] } where series is a single dimensional array of 
-    data values that is filled to full extent of the date range with future values filled with 
+/** this class is configured with { series : [] } where series is a single dimensional array of
+    data values that is filled to full extent of the date range with future values filled with
     nulls.
 **/
 Ext.define("RallyFunctions", function() {
@@ -16,16 +16,16 @@ Ext.define("RallyFunctions", function() {
             this.initConfig(config);
             return this;
         },
-    
+
     loadWsapiRecords: function(config,returnOperation){
         var deferred = Ext.create('Deft.Deferred');
         var me = this;
-                
+
         var default_config = {
             model: 'Defect',
             fetch: ['ObjectID']
         };
-        
+
         var full_config =  Ext.Object.merge(default_config,config);
         var store_class_name = 'Rally.data.wsapi.Store';
         if ( full_config.models ) {
@@ -46,7 +46,7 @@ Ext.define("RallyFunctions", function() {
         });
         return deferred.promise;
     },
-    
+
         _wsapiQuery : function( config , callback ) {
 
             var storeConfig = {
@@ -64,26 +64,28 @@ Ext.define("RallyFunctions", function() {
             };
             if (!_.isUndefined(config.context)) {
                 storeConfig.context = config.context;
-            }         
+            }
             Ext.create('Rally.data.WsapiDataStore', storeConfig);
         },
-   
+
         // create a filter based on a combination of release and/or iteration
-        createFilter : function( releaseName, iterationName, featureFieldName ) { 
+        createFilter : function( releaseName, iterationName, featureFieldName ) {
             var filter = Rally.data.wsapi.Filter.and([{property:'DirectChildrenCount',value:0}]);
 
             if (!_.isNull(releaseName)) {
-            	var filters = [{
+                var filters = [{
                     property: 'Release.Name',
                     value: releaseName
                 }];
-            	
-            	if ( featureFieldName ) {
-            		filters.push({
+
+                if ( featureFieldName ) {
+                    //filters.push({
+                    filters = [{
                         property: featureFieldName + '.Release.Name',
                         value: releaseName
-                    });
-            	}
+                    }];
+                    //});
+                }
                 filter = filter.and( Rally.data.wsapi.Filter.or(filters) );
             }
 
@@ -94,12 +96,12 @@ Ext.define("RallyFunctions", function() {
                     value: iterationName
                 });
 
-                filter = _.isNull(filter) ? ifilter : filter.and(ifilter);              
+                filter = _.isNull(filter) ? ifilter : filter.and(ifilter);
             }
             return filter;
         },
 
-        createFeatureFilter : function( releaseName ) { 
+        createFeatureFilter : function( releaseName ) {
             var filter = null;
 
             if (!_.isNull(releaseName)) {
@@ -117,7 +119,7 @@ Ext.define("RallyFunctions", function() {
             app.subscribe(app, 'timeboxReleaseChanged', app._timeboxChanged, app);
             app.subscribe(app, 'timeboxIterationChanged', app._timeboxChanged, app);
         }
-       
+
     };
-   
+
 });
